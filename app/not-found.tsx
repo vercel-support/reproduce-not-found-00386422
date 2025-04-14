f4c499308d59d57c98bb4b1d1d49220e6712974d
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-04-14 11:04:13 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { GetStaticProps } from 'next'
 
-const NotFound = () => (
+interface NotFoundProps {
+  message: string
+}
+
+const NotFound = ({ message }: NotFoundProps) => (
   <div>
-    <h1>APP ROUTER - 404 PAGE</h1>
+    <h1>APP ROUTER - 404 PAGE - NEW </h1>
     <p>This page is using the APP ROUTER</p>
+    <p>{message}</p>
   </div>
 )
 
-export default NotFound;
+export const getStaticProps: GetStaticProps<NotFoundProps> = async () => {
+  // You can fetch data here if needed
+  return {
+    props: {
+      message: "Custom message fetched at build time"
+    }
+  }
+}
+
+export default NotFound
